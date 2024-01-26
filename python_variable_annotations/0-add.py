378c5f81d5e1809609291a71d1462b27493ad5f2
jbizima <j.bizima@alustudent.com>
2024-01-26 08:23:51 +0200

    tasks

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,15 +1,7 @@
 #!/usr/bin/env python3
-<<<<<<< HEAD
 """"add a and b function"""
 
 
 def add(a: float, b: float) -> float:
     '''Add two floating numbers'''
-=======
-"""add function"""
-
-
-def add(a: float, b: float) -> float:
-    """Add two numbers"""
->>>>>>> 5c626b3fad84610f777769272a0c38ac716fd55b
     return a + b
