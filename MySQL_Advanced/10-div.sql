526dd262ae52bdcf077fb973b97e8efde35ae585
jbizima <j.bizima@alustudent.com>
2024-03-15 16:33:10 +0200

    edits

--- MySQL_Advanced/10-div.sql
+++ MySQL_Advanced/10-div.sql
@@ -1,18 +1,11 @@
 --SQL script that creates a function SafeDiv that divides (and returns) the first by the second number or returns 0 if the second number is equal to 0.
-DELIMITER //
+DELIMITER |
+
 DROP FUNCTION IF EXISTS SafeDiv;
-CREATE FUNCTION SafeDiv(
-       a INT,
-       b INT
-)
-RETURNS FLOAT
-DETERMINISTIC
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
 BEGIN
+	RETURN (IF (b = 0, 0, a / b));
+END |
 
-IF b = 0 THEN
-   RETURN 0;
-END IF;
-RETURN a / b;
-
-END;//
-DELIMITER ;
+DELIMITER;
